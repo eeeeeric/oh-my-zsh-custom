082484b6375d4275c898f40d76959d8ea89b221b
Eric Zheng <ericzheng@pingidentity.com>
2020-02-20 16:10:03 -0600

    Add bullet-train theme

---
+++ bullet-train-settings.zsh
@@ -0,0 +1,23 @@
+export BULLETTRAIN_STATUS_EXIT_SHOW=true
+export BULLETTRAIN_EXEC_TIME_SHOW=true
+export BULLETTRAIN_DIR_EXTENDED=2 # complete path
+export BULLETTRAIN_PROMPT_ORDER=(
+    time
+    status
+    custom
+    context
+    dir
+    screen
+    perl
+    ruby
+    virtualenv
+    nvm
+    aws
+    go
+    rust
+    elixir
+    git
+    kctx
+    hg
+    cmd_exec_time
+  )

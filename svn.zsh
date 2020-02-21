3faedaadafc506d95bf85cb3b38d0d100639ea85
Eric Zheng <ericzheng@pingidentity.com>
2020-02-21 15:03:23 -0600

    Create svn.zsh

---
+++ svn.zsh
@@ -0,0 +1,6 @@
+# Copy SVN diff to clipboard (the naming seems off, but that's due to an old, particular workflow)
+alias svn-cp-diff='svn diff | pbcopy'
+
+# Apply a patch file, possibly created from `svn diff`
+alias svn-apply-patch='patch -p0 -i'
+

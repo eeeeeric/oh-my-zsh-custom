979a40678109f1e87e8b0d75eab4e7d9e071d6d8
Eric Zheng <ericzheng@pingidentity.com>
2020-02-21 15:24:25 -0600

    Create python.zsh

---
+++ python.zsh
@@ -0,0 +1,9 @@
+# Python
+export PYTHONSTARTUP=~/.config/pythonrc
+
+#alias venv2='python2 /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/virtualenv.py'
+#alias mkvenv2='python2 /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/virtualenv.py venv'
+alias venv3='python3 -m venv'
+alias mkvenv3='python3 -m venv venv'
+
+alias serve='python2 -m SimpleHTTPServer 8080'

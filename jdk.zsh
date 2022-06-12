3c0393e1ccddb9dd495308dbff509538bdd2a65f
Eric Zheng <eeeeeric@users.noreply.github.com>
2022-06-12 17:17:44 -0500

    Update jdk.zsh

--- jdk.zsh
+++ jdk.zsh
@@ -3,8 +3,7 @@ set_java_version() {
 	export PATH="${JAVA_HOME}/bin:${PATH}"
 }
 
-export JAVA8_HOME="$(/usr/libexec/java_home -v 1.8)"
 export JAVA11_HOME="$(/usr/libexec/java_home -v 11)"
-export JAVA12_HOME="$(/usr/libexec/java_home -v 12)"
+export JAVA17_HOME="$(/usr/libexec/java_home -v 17)"
 
-set_java_version 1.8
+set_java_version 17
